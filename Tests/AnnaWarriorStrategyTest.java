0c7838ddd89543ec4079601468687ead24ee213b
Anna Huang <huanga@moravian.edu>
2023-03-29 17:27:27 -0400

    AnnaWarriorStrategy

---
+++ Tests/AnnaWarriorStrategyTest.java
@@ -0,0 +1,28 @@
+import Project02.*;
+import org.junit.jupiter.api.Test;
+import  static org.junit.jupiter.api.Assertions.*;
+
+public class AnnaWarriorStrategyTest {
+
+    @Test
+    public void TestMeWarriorStrategy(){
+        AnnaWarriorStrategy warrior = new AnnaWarriorStrategy();
+        People meWarrior = new AnnaWarrior("Minion","5", 59, warrior);
+        People otherWarrior = new AnnaWarrior("Idiot", "5", 60, warrior);
+
+        assertEquals(58,warrior.strategy(meWarrior,otherWarrior));
+
+
+   }
+
+
+   @Test
+    public void TestOtherWarriorStrategy(){
+        AnnaWarriorStrategy warrior = new AnnaWarriorStrategy();
+        People meWarrior = new AnnaWarrior("Minion", "4", 59, warrior);
+        People otherWarrior = new AnnaWarrior("Idiot", "4", 90, warrior);
+
+        assertEquals(80,warrior.strategy(meWarrior, otherWarrior));
+   }
+
+}
