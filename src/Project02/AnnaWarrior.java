0c339ce9fc9a34af95cd294d68ba65b99ee58d58
Anna Huang <huanga@moravian.edu>
2023-03-29 17:13:32 -0400

    AnnaWarrior

---
+++ src/Project02/AnnaWarrior.java
@@ -0,0 +1,16 @@
+package Project02;
+
+public class AnnaWarrior extends People {
+    public AnnaWarrior(String nation, String tribe, int lifePoints, Strategy AnnaWarriorStrategy)
+    {
+        super(nation, tribe, PeopleType.warrior, lifePoints, AnnaWarriorStrategy);
+        myDescription = "\tAnna Warrior";
+    }
+
+    @Override
+    public int encounterLifePoints(People me, People otherPerson)
+    {
+        return encounterStrategy.strategy(me, otherPerson);
+    }
+
+}
