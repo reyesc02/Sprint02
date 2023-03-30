71647ac07b163042add8a3d61d7585ad84720a62
Anna Huang <huanga@moravian.edu>
2023-03-29 23:14:46 -0400

    WizardStrategyUpdate

--- Tests/AnnaWizardStrategyTest.java
+++ Tests/AnnaWizardStrategyTest.java
@@ -1,3 +1,4 @@
+
 import Project02.*;
 import org.junit.jupiter.api.Test;
 import  static org.junit.jupiter.api.Assertions.*;
@@ -9,7 +10,7 @@ public class AnnaWizardStrategyTest {
     {
         AnnaWizardStrategy wizard = new AnnaWizardStrategy();
         People meWizard = new AnnaWizard("Idiot", "3", 90, wizard);
-        People otherWizard = new AnnaWarrior("Minion", "3", 80, wizard);
+        People otherWizard = new AnnaWizard("Minion", "3", 80, wizard);
 
         assertEquals(30, wizard.strategy(meWizard, otherWizard));
 
@@ -18,18 +19,18 @@ public class AnnaWizardStrategyTest {
     public void  testWizardStrategyWhenMePointsLessThanOtherPoints(){
         AnnaWizardStrategy wizard = new AnnaWizardStrategy();
         People meWizard = new AnnaWizard("Minion", "3", 80, wizard);
-        People otherWarrior = new AnnaWarrior("Idiot", "3", 90, wizard);
+        People otherWarrior = new AnnaWizard("Idiot", "3", 90, wizard);
         assertEquals(79,wizard.strategy(meWizard,otherWarrior));
     }
 
     @Test
     public void TestMeWizardStrategyHeal()
     {
-        AnnaWarriorStrategy wizard = new AnnaWarriorStrategy();
-        People meWizard = new AnnaWizard("Idiots", "2", 50, wizard);
-        People otherWizard = new AnnaWarrior("Minion", "2", 70, wizard);
+        AnnaWizardStrategy wizard = new AnnaWizardStrategy();
+        People meWizard = new AnnaWizard("Idiots", "2", 39, wizard);
+        People otherWizard = new AnnaWizard("Minion", "4", 60, wizard);
 
-        assertEquals(60,wizard.strategy(meWizard, otherWizard));
+        assertEquals(89,wizard.strategy(meWizard, otherWizard));
 
     }
 }
