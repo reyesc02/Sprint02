38c24b9361d91ba00e2a112e4b43e6086f2ae261
Anna Huang <huanga@moravian.edu>
2023-03-30 08:40:03 -0400

    Changes 2

--- Tests/ShawnWarriorStrategyTest.java
+++ Tests/ShawnWarriorStrategyTest.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ShawnWarriorStrategyTest {
+import Project02.*;
+
+import org.junit.jupiter.api.Test;
+import  static org.junit.jupiter.api.Assertions.*;
+//
+public class ShawnWarriorStrategyTest {
+
+    @Test
+    public void TestMeWarriorStrategy(){
+        ShawnWarriorStrategy warrior = new ShawnWarriorStrategy();
+        People meWarrior = new ShawnWarrior("Minion","5", 59, warrior);
+        People otherWarrior = new ShawnWarrior("Idiot", "5", 60, warrior);
+
+        assertEquals(58,warrior.strategy(meWarrior,otherWarrior));
+
+
+    }
+
+
+    @Test
+    public void TestOtherWarriorStrategy(){
+        ShawnWarriorStrategy warrior = new ShawnWarriorStrategy();
+        People meWarrior = new ShawnWarrior("Minion", "4", 59, warrior);
+        People otherWarrior = new ShawnWarrior("Idiot", "4", 90, warrior);
+
+        assertEquals(80,warrior.strategy(meWarrior, otherWarrior));
+    }
+
 }
