f1e164f89154f0abdef03697b82a79681c2c5faf
Anna Huang <huanga@moravian.edu>
2023-04-30 21:04:52 -0400

    Update Healer

--- src/Project02/AnnaHealerStrategy.java
+++ src/Project02/AnnaHealerStrategy.java
@@ -2,24 +2,22 @@ package Project02;
 
 public class AnnaHealerStrategy implements Strategy {
     @Override
-    public int strategy(People me, People otherPerson) {
+    public int strategy(People me, People otherPerson)
+    {
+        Die die = new Die(10);
+        die.roll();
         if (me.getNation() == otherPerson.getNation() && me.getTribe() == otherPerson.getTribe())
         {
-            if (me.getType() == PeopleType.wizard)
+            if (me.getType() == PeopleType.wizard && otherPerson.getType() == PeopleType.wizard)
             {
-
                 if (me.getLifePoints() < otherPerson.getLifePoints())
                 {
-                    int heal = otherPerson.getLifePoints() / 2 + me.getLifePoints();
+                    int heal = otherPerson.getLifePoints() / 2 + me.getLifePoints() + die.roll();
                     return heal;
                 } else
                     return me.getLifePoints();
             }
-            else if (me.getType() == PeopleType.warrior) {
-                return 66;
-            } else if (me.getType() == PeopleType.healer) {
-                return 20;
-            }
+        }
+        return me.getLifePoints();
     }
-        return 0;
-}}
+}
